a63ad0ac49e432a0e720f2938dd253a75c81dfba
Mahendra Katoch <kmkatoch@gmail.com>
2019-07-22 17:49:26 +0100

    Added Address entity

---
+++ src/main/java/com/lama/data/model/Address.java
@@ -0,0 +1,34 @@
+package com.lama.data.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @NotBlank(message = "First Line of Address is mandatory")
+    private String addressFirst;
+
+    @NotBlank(message = "Second Line of Address is mandatory")
+    private String addressSecond;
+
+    @NotBlank(message = "City is mandatory")
+    private String city;
+
+    @NotBlank(message = "State is mandatory")
+    private String state;
+
+    @NotBlank(message = "Pin Code is mandatory")
+    private String pinCode;
+}
--- src/main/java/com/lama/data/model/Contact.java
+++ src/main/java/com/lama/data/model/Contact.java
@@ -5,7 +5,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
@@ -21,23 +20,8 @@ public class Contact {
     private int phoneNumber;
 
     @OneToOne
-    @JoinColumn(name = "country_id", nullable = false)
-    private Country country;
-
-    @NotBlank(message = "First Line of Address is mandatory")
-    private String addressFirst;
-
-    @NotBlank(message = "Second Line of Address is mandatory")
-    private String addressSecond;
-
-    @NotBlank(message = "City is mandatory")
-    private String city;
-
-    @NotBlank(message = "State is mandatory")
-    private String state;
-
-    @NotBlank(message = "Pin Code is mandatory")
-    private String pinCode;
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
 
     @CreationTimestamp
     private Date createdAt;
